451d3bcb64a6130931a15ed6ca5da8641ddf37c5
Katerina Davidova <kat@moucha.cc>
2021-05-28 20:45:50 +0200

    ostran dvojite uvozovky

--- index.js
+++ index.js
@@ -1,35 +1,34 @@
-const btnForwardEl = document.querySelector(".js-btn-forward");
-const btnBackEl = document.querySelector(".js-btn-back");
+const btnForwardEl = document.querySelector('.js-btn-forward');
+const btnBackEl = document.querySelector('.js-btn-back');
 
-btnForwardEl.addEventListener("click", () => {
+btnForwardEl.addEventListener('click', () => {
   const currentQ = getCurrentQuestion();
   changeQuestion(currentQ, currentQ + 1);
 });
 
-btnBackEl.addEventListener("click", () => {
+btnBackEl.addEventListener('click', () => {
   const currentQ = getCurrentQuestion();
   changeQuestion(currentQ, currentQ - 1);
 });
 
 function getCurrentQuestion() {
-  const gameEl = document.getElementById("game");
+  const gameEl = document.getElementById('game');
   return Number(gameEl.dataset.currentQ);
 }
 
 function setCurrentQuestion(question) {
-  const gameEl = document.getElementById("game");
+  const gameEl = document.getElementById('game');
   gameEl.dataset.currentQ = question;
 }
 
 function setQestionIndicator(num) {
-  num.style.color = "red";
-  const indicatorEl = document.querySelector(".js-q-indicator");
+  const indicatorEl = document.querySelector('.js-q-indicator');
   indicatorEl.textContent = `${num}/6`;
 }
 
 function changeQuestion(from, to) {
   const inputEls = Array.from(
-    document.querySelectorAll(`.js-game-q${from} input`)
+    document.querySelectorAll(`.js-game-q${from} input`),
   );
   const chekedItems = inputEls.filter((item) => {
     if (item.checked === true) {
@@ -38,16 +37,16 @@ function changeQuestion(from, to) {
   });
 
   if (chekedItems.length > 0) {
-    document.querySelector(`.js-game-q${from}`).classList.add("hide");
-    document.querySelector(`.js-game-q${to}`).classList.remove("hide");
+    document.querySelector(`.js-game-q${from}`).classList.add('hide');
+    document.querySelector(`.js-game-q${to}`).classList.remove('hide');
     setCurrentQuestion(to);
     setQestionIndicator(to);
   } else {
-    alert("zaškrtni položku");
+    alert('zaškrtni položku');
   }
 }
 
-import { Aktualne } from "./Zmrzliny/index.js";
+import { Aktualne } from './Zmrzliny/index.js';
 Aktualne();
 
 const datum = new Date();
@@ -55,5 +54,5 @@ const den = datum.getDate();
 const mesic = datum.getMonth() + 1;
 const rok = datum.getFullYear();
 
-const datumElm = document.querySelector(".aktu-na-cepu");
+const datumElm = document.querySelector('.aktu-na-cepu');
 datumElm.innerHTML = `Dneska ${den}. ${mesic}. ${rok} zmrz.li`;
