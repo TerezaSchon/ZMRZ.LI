b6cef5deb3cd0afa672825de45975e5e0e3b5d9b
klarahapkova <80477797+klarahapkova@users.noreply.github.com>
2021-05-28 18:00:40 +0200

    Drobné změny

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
-const btnForwardEl = document.querySelector('.js-btn-forward');
-const btnBackEl = document.querySelector('.js-btn-back');
+const btnForwardEl = document.querySelector(".js-btn-forward");
+const btnBackEl = document.querySelector(".js-btn-back");
 
-btnForwardEl.addEventListener('click', () => {
+btnForwardEl.addEventListener("click", () => {
   const currentQ = getCurrentQuestion();
   changeQuestion(currentQ, currentQ + 1);
 });
 
-btnBackEl.addEventListener('click', () => {
+btnBackEl.addEventListener("click", () => {
   const currentQ = getCurrentQuestion();
   changeQuestion(currentQ, currentQ - 1);
 });
 
 function getCurrentQuestion() {
-  const gameEl = document.getElementById('game');
+  const gameEl = document.getElementById("game");
   return Number(gameEl.dataset.currentQ);
 }
 
 function setCurrentQuestion(question) {
-  const gameEl = document.getElementById('game');
+  const gameEl = document.getElementById("game");
   gameEl.dataset.currentQ = question;
 }
 
 function setQestionIndicator(num) {
-  num.style.color = 'red';
-  const indicatorEl = document.querySelector('.js-q-indicator');
+  num.style.color = "red";
+  const indicatorEl = document.querySelector(".js-q-indicator");
   indicatorEl.textContent = `${num}/6`;
 }
 
 function changeQuestion(from, to) {
   const inputEls = Array.from(
-    document.querySelectorAll(`.js-game-q${from} input`),
+    document.querySelectorAll(`.js-game-q${from} input`)
   );
   const chekedItems = inputEls.filter((item) => {
     if (item.checked === true) {
@@ -38,11 +38,14 @@ function changeQuestion(from, to) {
   });
 
   if (chekedItems.length > 0) {
-    document.querySelector(`.js-game-q${from}`).classList.add('hide');
-    document.querySelector(`.js-game-q${to}`).classList.remove('hide');
+    document.querySelector(`.js-game-q${from}`).classList.add("hide");
+    document.querySelector(`.js-game-q${to}`).classList.remove("hide");
     setCurrentQuestion(to);
     setQestionIndicator(to);
   } else {
-    alert('zaškrtni položku');
+    alert("zaškrtni položku");
   }
 }
+
+import { Aktualne } from "./Zmrzliny/index.js";
+Aktualne();
--- zmrzliny.js
+++ zmrzliny.js
@@ -1,204 +1,197 @@
-
-
-const zmrzliny = [
+export const zmrzliny = [
   {
-    id: 1,
+    id: "prvni",
     name: "vanilka - rum",
     color: "#fff200",
   },
   {
-    id: 2,
+    id: "druhy",
     name: "slaný karamel",
     color: "#ee7b22",
   },
   {
-    id: 3,
+    id: "treti",
     name: "bezinka",
     color: "#dfc07d",
   },
   {
-    id: 4,
+    id: "ctvrty",
     name: "tmavá čokoláda - uzená sůl",
     color: "#3c2415",
   },
   {
-    id: 5,
+    id: "paty",
     name: "mléčný čokoládový shake",
     color: "#754c29",
   },
   {
-    id: 6,
-    name: "banán - arašído slaninová směs",
+    id: "sesty",
+    name: "banán – arašídovo slaninová směs",
     color: "#f7941d",
   },
   {
-    id: 7,
-    name: "bourbor - sušenky s kukuřičných lupínků",
+    id: "sedmy",
+    name: "bourbon - sušenky z kukuřičných lupínků",
     color: "#2b3990",
   },
   {
-    id: 8,
+    id: "osmy",
     name: "káva arabica",
     color: "#8b5e3c",
   },
   {
-    id: 9,
+    id: "devaty",
     name: "oolong čaj",
     color: "#71732f",
   },
   {
-    id: 10,
+    id: "desaty",
     name: "masala čaj",
     color: "#e12b27",
   },
   {
-    id: 11,
+    id: "jedenacty",
     name: "černé pivo",
     color: "#231f20",
   },
   {
-    id: 12,
+    id: "dvanacty",
     name: "med - grahamové sušenky",
     color: "#f79441",
   },
   {
-    id: 13,
+    id: "trinacty",
     name: "zázvor v řeckém jogurtu",
     color: "#469172",
   },
   {
-    id: 14,
+    id: "ctrnacty",
     name: "bílá čokoláda s mákem",
     color: "#c2b59b",
   },
   {
-    id: 15,
+    id: "patnacty",
     name: "pražená bílá čokoláda - levandule",
     color: "#7f3f98",
   },
   {
-    id: 16,
-    name: "borůvky v řeckém jogurtu - levandule",
+    id: "sestnacty",
+    name: "borůvky v řeckém jogurtu – levandule",
     color: "#4c2d85",
   },
   {
-    id: 17,
+    id: "sedmnacty",
     name: "meruňka - červený rooibos",
     color: "#f26522",
   },
   {
-    id: 18,
+    id: "osmnacty",
     name: "broskev - černý čaj",
     color: "#f26522",
   },
   {
-    id: 19,
-    name: "ananas - zelený čaj s jasmínem",
+    id: "devatenacty",
+    name: "ananas – zelený čaj s jasmínem",
     color: "#cbd63f",
   },
   {
-    id: 20,
-    name: "máta & pepř - kousky čokolády",
+    id: "dvacaty",
+    name: "máta & pepř – kousky čokolády",
     color: "#00a79d",
   },
   {
-    id: 21,
+    id: "jednadvacaty",
     name: "šípek",
     color: "#a32823",
   },
   {
-    id: 22,
-    name: "smetanový sýr - ostružinový výr",
+    id: "dvaadvacaty",
+    name: "smetanový sýr – ostružinový sýr",
     color: "#d8b898",
   },
   {
-    id: 23,
-      name: "donut",
-      color: "#dfc07d",
-    },
+    id: "triadvacaty",
+    name: "donut",
+    color: "#dfc07d",
+  },
   {
-    id: 24,
-    name: "leche merengada - citronová tříšť",
+    id: "ctyriadvacaty",
+    name: "leche merengada – citronová tříšť",
     color: "#1b75bc",
   },
   {
-    id: 25,
-    name: "pomerančový sorbet - šafrán",
+    id: "petadvacaty",
+    name: "pomerančový sorbet – šafrán",
     color: "#f15a29",
   },
   {
-    id: 26,
-    name: "hrušky & rýnský ryzlink sorbet - zázvor",
+    id: "sestadvacaty",
+    name: "hrušky & rýnský ryzlink sorbet – zázvor",
     color: "#8dc63f",
   },
   {
-    id: 27,
+    id: "sedmadvacaty",
     name: "merlot & cola sorbet",
     color: "#601d25",
   },
   {
-    id: 28,
-    name: "kokos & limetka sorbet - chilli",
+    id: "osmadvacaty",
+    name: "kokos & limetka sorbet – chilli",
     color: "#00a14b",
   },
   {
-    id: 29,
-    name: "citron sorbet - bazalka",
+    id: "devetadvacaty",
+    name: "citron sorbet – bazalka",
     color: "#51b748",
   },
   {
-    id: 30,
-    name: "maliny - šampaňské sorbet",
+    id: "tricaty",
+    name: "maliny – šampaňské sorbet",
     color: "#eb284c",
   },
+];
 
-
-]
-
-const gameAnswers = {
-      q1: {
-        a1: [ 13, 4, "" ],
-        a2: [ "", "", "" ],
-        a3: [ "", "", "" ],
-        a4: [ "", "", "" ],
-        a5: [ "", "", "" ],
-      },
-      q2: {
-        a1: [ 13, 4, "" ],
-        a2: [ "", "", "" ],
-        a3: [ "", "", "" ],
-        a4: [ "", "", "" ],
-        a5: [ "", "", "" ],
-      },
-      q3: {
-        a1: [ 13, 4, "" ],
-        a2: [ "", "", "" ],
-        a3: [ "", "", "" ],
-        a4: [ "", "", "" ],
-        a5: [ "", "", "" ],
-    }
-      q4: {
-        a1: [ 13, 4, "" ],
-        a2: [ "", "", "" ],
-        a3: [ "", "", "" ],
-        a4: [ "", "", "" ],
-        a5: [ "", "", "" ],
-    }
-      q5: {
-        a1: [ 13, 4, "" ],
-        a2: [ "", "", "" ],
-        a3: [ "", "", "" ],
-        a4: [ "", "", "" ],
-        a5: [ "", "", "" ],
-      }
-      q5: {
-        a1: [ 13, 4, "" ],
-        a2: [ "", "", "" ],
-        a3: [ "", "", "" ],
-        a4: [ "", "", "" ],
-        a5: [ "", "", "" ],
-      }
-    }
-
-
-
+// const gameAnswers = {
+//       q1: {
+//         a1: [ 13, 4, "" ],
+//         a2: [ "", "", "" ],
+//         a3: [ "", "", "" ],
+//         a4: [ "", "", "" ],
+//         a5: [ "", "", "" ],
+//       },
+//       q2: {
+//         a1: [ 13, 4, "" ],
+//         a2: [ "", "", "" ],
+//         a3: [ "", "", "" ],
+//         a4: [ "", "", "" ],
+//         a5: [ "", "", "" ],
+//       },
+//       q3: {
+//         a1: [ 13, 4, "" ],
+//         a2: [ "", "", "" ],
+//         a3: [ "", "", "" ],
+//         a4: [ "", "", "" ],
+//         a5: [ "", "", "" ],
+//     }
+//       q4: {
+//         a1: [ 13, 4, "" ],
+//         a2: [ "", "", "" ],
+//         a3: [ "", "", "" ],
+//         a4: [ "", "", "" ],
+//         a5: [ "", "", "" ],
+//     }
+//       q5: {
+//         a1: [ 13, 4, "" ],
+//         a2: [ "", "", "" ],
+//         a3: [ "", "", "" ],
+//         a4: [ "", "", "" ],
+//         a5: [ "", "", "" ],
+//       }
+//       q5: {
+//         a1: [ 13, 4, "" ],
+//         a2: [ "", "", "" ],
+//         a3: [ "", "", "" ],
+//         a4: [ "", "", "" ],
+//         a5: [ "", "", "" ],
+//       }
+//     };
