5a8e3bf401c3cd0f80b271cf170bee412f1183b4
TerezaSchon <tereza.vlachova87@gmail.com>
2021-05-29 19:30:58 +0200

    zavření menu po kliku na křížek a pokus o zavření menu po kliku na záložku

--- Header.js
+++ Header.js
@@ -1,7 +1,22 @@
-const menuBtn = document.querySelector('#menu__btn');
+/* rozklikni menu */
+const menuBtn1 = document.querySelector('#menu__btn1');
 const menu = document.querySelector('#menu');
 const menuItems = menu.querySelectorAll('#menu__item');
 
-menuBtn.addEventListener('click', () => {
-  menu.classList.toggle('menu-closed');
+menuBtn1.addEventListener('click', () => {
+  menu.classList.remove('menu-closed');
+});
+
+/* zavři menu po kliku na křížek */
+const menuBtn2 = document.querySelector('#menu__btn2');
+
+menuBtn2.addEventListener('click', () => {
+  menu.classList.add('menu-closed');
+});
+
+/* zavři menu po kliknutí na záložku */
+const closeMenu = document.querySelector('.item');
+
+closeMenu.addEventListener('click', () => {
+  menu.classList.add('menu-closed');
 });
