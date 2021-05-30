2aba07492a5ddd2c13ff1a2046e960f0d9ef169d
TerezaSchon <tereza.vlachova87@gmail.com>
2021-05-30 12:10:38 +0200

    zjednodušen kód na klikání v menu

--- Header.js
+++ Header.js
@@ -1,7 +1,6 @@
 /* rozklikni menu */
 const menuBtn1 = document.querySelector('#menu__btn1');
 const menu = document.querySelector('#menu');
-const menuItems = menu.querySelectorAll('#menu__item');
 
 menuBtn1.addEventListener('click', () => {
   menu.classList.remove('menu-closed');
@@ -16,29 +15,38 @@ menuBtn2.addEventListener('click', () => {
 
 /* zavři menu po kliknutí na libovolnou záložku */
 
-const closeMenu1 = document.querySelector('#menu__item--1');
+const menuItems = document.querySelectorAll('#menu .item');
+
+menuItems.forEach((item) => {
+  item.addEventListener('click', () => {
+    menu.classList.add('menu-closed');
+  });
+});
+
+/* const closeMenu1 = document.querySelector('#menu__item--1');
 
 closeMenu1.addEventListener('click', () => {
   menu.classList.add('menu-closed');
 });
 
-/*  */
+
 const closeMenu2 = document.querySelector('#menu__item--2');
 
 closeMenu2.addEventListener('click', () => {
   menu.classList.add('menu-closed');
 });
 
-/*  */
+
 const closeMenu3 = document.querySelector('#menu__item--3');
 
 closeMenu3.addEventListener('click', () => {
   menu.classList.add('menu-closed');
 });
 
-/*  */
+
 const closeMenu4 = document.querySelector('#menu__item--4');
 
 closeMenu4.addEventListener('click', () => {
   menu.classList.add('menu-closed');
 });
+ */
